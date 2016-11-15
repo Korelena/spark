de4df88f07905a959545042a9ec486cf37e057df
Korelena <elena.korshunova@gmail.com>
2016-11-15 21:35:52 +0200

    Style editing

--- mllib/src/main/scala/org/apache/spark/mllib/api/python/DependenceClusteringModelWrapper.scala
+++ mllib/src/main/scala/org/apache/spark/mllib/api/python/DependenceClusteringModelWrapper.scala
@@ -17,14 +17,15 @@
 
 package org.apache.spark.mllib.api.python
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.mllib.clustering.DependenceClusteringModel
+import org.apache.spark.rdd.RDD
 
 /**
  * A Wrapper of DependenceClusteringModel to provide helper method for Python
  */
 private[python] class DependenceClusteringModelWrapper(model: DependenceClusteringModel)
-  extends DependenceClusteringModel(model.t, model.epsi_d, model.delta_dep, model.delta_e, model.delta_v, model.assignments) {
+  extends DependenceClusteringModel(model.t, model.epsi_d, model.delta_dep, 
+                                    model.delta_e, model.delta_v, model.assignments) {
 
   def getAssignments: RDD[Array[Any]] = {
     model.assignments.map(x => Array(x.id, x.cluster))
